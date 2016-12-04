d9008a680bfe63c90eaa0965818a23e87a65952f
Nate Foss <npfoss@gmail.com>
2016-12-03 22:08:36 -0500

    set up pagerank

--- teamRank.py
+++ teamRank.py
@@ -3,24 +3,44 @@
 # |                               Nate Foss                                   |
 # |                                12/3/16                                    |
 # +---------------------------------------------------------------------------+
-
+from numpy import dot
 ###################################START OF PROGRAM#############################
 #-------------------------------------------------------------------Team Rank---
 def retrieveAndInputData():
     regionsAndTeams = dict()
     regionsAndTeams['r1'] = ['MIT','Tufts','Williams','TJ',]
-    weightedVictoryGraph = [[0,2,1,0],
-                            [0,0,1,0],
+    weightedVictoryGraph = [[0,2,2,1],
+                            [1,0,2,1],
                             [1,1,0,0],
-                            [1,1,1,0]]
+                            [1,1,2,0]]
     return regionsAndTeams , weightedVictoryGraph
 #-------------------------------------------------------------------Team Rank---
 def printMatrix(matrix):
     for r in range(len(matrix)):
         print(matrix[r])
 #-------------------------------------------------------------------Team Rank---
-def PageRank(matrix):
-    
+def normalizeMatrix(matrix):
+    for c in range(len(matrix)):
+        s = 0
+        for r in range(len(matrix)):
+            s += matrix[r][c]
+        if s > 0:
+            for r in range(len(matrix)):
+                matrix[r][c] = matrix[r][c] *1.0 / s
+#-------------------------------------------------------------------Team Rank---
+def PageRank(matrix, iterationStep = 15):
+    # matrix should be normalized
+    start = [1.0]*len(matrix)
+    i = 1
+    while True:
+        if i % iterationStep == 0:
+            print(start)
+            if input('continue? (type "n" to stop) ').strip() == 'n':
+                break
+        print(sum(start), start)
+        start = list(dot(matrix, start))
+        i += 1
+
 
 #======================< GLOBAL STUFFS >============================Team Rank===
 
@@ -33,9 +53,10 @@ def main():
     regionsAndTeams , weightedVictoryGraph = retrieveAndInputData()
 
     printMatrix(weightedVictoryGraph)
+    normalizeMatrix(weightedVictoryGraph)
+    printMatrix(weightedVictoryGraph)
 
-
-
+    PageRank(weightedVictoryGraph)
 
 #-------------------------------------------------------------------Team Rank---
 if __name__ == '__main__': from time import clock; START_TIME = clock();  main(); \
