1b69ce1697d8464b15fa8f60d7e6130241d8f695
Nate Foss <npfoss@gmail.com>
2016-12-04 01:17:40 -0500

    lots of improvements: undefeated teams don't break it, no longer an advantage for competing in more meets, transposed matrix so lower score is better and the weird edge effects are at the bottom instead of top

--- teamRank.py
+++ teamRank.py
@@ -3,7 +3,7 @@
 # |                               Nate Foss                                   |
 # |                                12/3/16                                    |
 # +---------------------------------------------------------------------------+
-from numpy import dot
+from numpy import dot , transpose
 ###################################START OF PROGRAM#############################
 #-------------------------------------------------------------------Team Rank---
 def retrieveAndInputData():
@@ -13,11 +13,24 @@ def retrieveAndInputData():
                             [1,0,2,1],
                             [1,1,0,0],
                             [1,1,2,0]]
+    # weightedVictoryGraph = [[0,1,2,1],
+    #                         [1,0,2,2],
+    #                         [0,1,0,4],
+    #                         [0,1,2,0]]
+    weightedVictoryGraph = [list(elt) for elt in transpose(weightedVictoryGraph)] # now lowest score is best and poeple point to people they beat
     return regionsAndTeams , weightedVictoryGraph
 #-------------------------------------------------------------------Team Rank---
 def printMatrix(matrix):
     for r in range(len(matrix)):
         print(matrix[r])
+#-------------------------------------------------------------------Team Rank---
+def adjustForNumMeets(matrix):
+    for r in range(len(matrix)):
+        for c in range(r):
+            s = 1.0*(matrix[r][c] + matrix[c][r])
+            if s:
+                matrix[r][c] /= s
+                matrix[c][r] /= s
 #-------------------------------------------------------------------Team Rank---
 def normalizeMatrix(matrix):
     for c in range(len(matrix)):
@@ -28,20 +41,34 @@ def normalizeMatrix(matrix):
             for r in range(len(matrix)):
                 matrix[r][c] = matrix[r][c] *1.0 / s
 #-------------------------------------------------------------------Team Rank---
+def addFudgeFactor(matrix, factor):
+    for r in range(len(matrix)):
+        for c in range(len(matrix)):
+            matrix[r][c] = matrix[r][c]*(1-factor) + factor / len(matrix)
+#-------------------------------------------------------------------Team Rank---
+def adjustMatrixToUseNet(matrix):
+#---******** WARNING ****** this is a mistake, don't do it
+    for r in range(len(matrix)):
+        for c in range(r):
+            if matrix[r][c] > matrix[c][r]:
+                matrix[r][c] -= matrix[c][r]
+                matrix[c][r] = 0
+            else:
+                matrix[c][r] -= matrix[r][c]
+                matrix[r][c] = 0
+#-------------------------------------------------------------------Team Rank---
 def PageRank(matrix, iterationStep = 15):
     # matrix should be normalized
     start = [1.0]*len(matrix)
     i = 1
     while True:
         if i % iterationStep == 0:
-            print(start)
+            #print(start)
             if input('continue? (type "n" to stop) ').strip() == 'n':
                 break
         print(sum(start), start)
         start = list(dot(matrix, start))
         i += 1
-
-
 #======================< GLOBAL STUFFS >============================Team Rank===
 
 #===============================< MAIN >========================================
@@ -53,8 +80,16 @@ def main():
     regionsAndTeams , weightedVictoryGraph = retrieveAndInputData()
 
     printMatrix(weightedVictoryGraph)
+    print()
+    adjustForNumMeets(weightedVictoryGraph)
+    printMatrix(weightedVictoryGraph)
+    print()
     normalizeMatrix(weightedVictoryGraph)
     printMatrix(weightedVictoryGraph)
+    print()
+    addFudgeFactor(weightedVictoryGraph, .1)
+    printMatrix(weightedVictoryGraph)
+    print()
 
     PageRank(weightedVictoryGraph)
 
